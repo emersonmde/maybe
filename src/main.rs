70c9511995547294eaa352a2878364dbf06162d4
Matthew Emerson <emersonmde@protonmail.com>
2023-03-02 08:55:54 -0500

    Removed generic types and implemented node hashing

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,22 @@
-// mod prolly_tree;
-mod node;
 mod kvstore;
+mod node;
 
-use std::collections::{HashMap, BTreeMap};
 use core::hash::Hash;
+use std::collections::{BTreeMap, HashMap};
 
 use kvstore::KVStore;
 use node::Node;
-
-
 
 #[derive(Debug)]
 struct MemoryStore<K, V> {
     data: HashMap<K, V>,
 }
 
-impl<K, V> KVStore<K, V> for MemoryStore<K, V> 
+impl<K, V> KVStore<K, V> for MemoryStore<K, V>
 where
     K: Eq + Hash + Clone,
     V: Clone,
 {
-    
     fn new() -> Self {
         Self {
             data: HashMap::new(),
@@ -37,34 +33,29 @@ where
 }
 
 #[derive(Debug)]
-struct ProllyTree<K, V> {
-    root: Node<K, V>,
+struct ProllyTree {
+    root: Node,
 }
 
-impl<K, V> ProllyTree<K, V>
-where
-    K: Ord + Clone,
-    V: Clone,
-{
+impl ProllyTree {
     fn new() -> Self {
         Self {
             root: Node::Leaf {
-                hash: "".to_string(),
+                hash_value: "".to_string(),
                 data: BTreeMap::new(),
             },
         }
     }
 
-    fn get(&self, key: K) -> Option<V> {
+    fn get(&self, key: Vec<u8>) -> Option<Vec<u8>> {
         self.root.get(key)
     }
 
-    fn put(&mut self, key: K, value: V) {
+    fn put(&mut self, key: Vec<u8>, value: Vec<u8>) {
         self.root.put(key, value);
-        let chunks = self.root.chunk();
+        let _chunks = self.root.chunk();
     }
 }
-
 
 fn main() {
     let mut storage: MemoryStore<Vec<u8>, Vec<u8>> = MemoryStore::new();
@@ -75,9 +66,13 @@ fn main() {
     }
     println!("Storage {:?}", storage);
 
-
-
-    // let mut tree = prolly_tree::ProllyTree::new();
-    // tree.put(vec![2, 4], vec![8, 9]);
-    // println!("Testing {:?}", tree.get(vec![2, 4]).unwrap());
+    let mut tree: ProllyTree = ProllyTree::new();
+    tree.put(vec![2, 4], vec![8, 9]);
+    println!("Tree {:?}", tree);
+    println!("2, 4 {:?}", tree.get(vec![2, 4]).unwrap());
+    println!(
+        "key1 {:?} {:?}",
+        String::from("key1").as_bytes().to_vec(),
+        tree.get(String::from("key1").as_bytes().to_vec())
+    );
 }
--- src/node.rs
+++ src/node.rs
@@ -1,32 +1,28 @@
+use crate::kvstore::KVStore;
+use sha2::{Digest, Sha256};
 use std::collections::BTreeMap;
 
-use crate::kvstore::KVStore;
-
 #[derive(Debug)]
-pub enum Node<K, V> {
+pub enum Node {
     Leaf {
-        hash: String,
-        data: BTreeMap<K, V>,
+        hash_value: String,
+        data: BTreeMap<Vec<u8>, Vec<u8>>,
     },
     Branch {
-        hash: String,
-        children: Vec<Node<K, V>>,
+        hash_value: String,
+        children: Vec<Node>,
     },
 }
 
-impl<K, V> KVStore<K, V> for Node<K, V>
-where
-    K: Ord + Clone,
-    V: Clone,
-{
+impl KVStore<Vec<u8>, Vec<u8>> for Node {
     fn new() -> Self {
         Self::Leaf {
-            hash: "".to_string(),
+            hash_value: "".to_string(),
             data: BTreeMap::new(),
         }
     }
 
-    fn get(&self, key: K) -> Option<V> {
+    fn get(&self, key: Vec<u8>) -> Option<Vec<u8>> {
         match self {
             Node::Leaf { data, .. } => data.get(&key).cloned(),
             Node::Branch { children, .. } => {
@@ -42,7 +38,7 @@ where
         }
     }
 
-    fn put(&mut self, key: K, value: V) {
+    fn put(&mut self, key: Vec<u8>, value: Vec<u8>) {
         match self {
             Node::Leaf { data, .. } => {
                 data.insert(key.clone(), value);
@@ -53,23 +49,20 @@ where
                 }
             }
         }
+        self.calc_hash();
     }
 }
 
-impl<K, V> Node<K, V>
-where
-    K: Ord + Clone,
-    V: Clone,
-{
-    pub fn chunk(&mut self) -> Vec<Node<K, V>> {
+impl Node {
+    pub fn chunk(&mut self) -> Vec<Node> {
         match self {
             Node::Leaf { data, .. } => {
                 let mut left = Node::Leaf {
-                    hash: "".to_string(),
+                    hash_value: "".to_string(),
                     data: BTreeMap::new(),
                 };
                 let mut right = Node::Leaf {
-                    hash: "".to_string(),
+                    hash_value: "".to_string(),
                     data: BTreeMap::new(),
                 };
                 let mut i = 0;
@@ -83,7 +76,39 @@ where
                 }
                 vec![left, right]
             }
-            Node::Branch { .. } => todo!()
+            Node::Branch { .. } => todo!(),
+        }
+    }
+
+    fn calc_hash(&mut self) -> String {
+        let mut hasher = Sha256::new();
+        match self {
+            Node::Leaf { data, hash_value } => {
+                for (key, value) in data {
+                    hasher.update(key);
+                    hasher.update(value);
+                }
+                *hash_value = format!("{:x}", hasher.finalize());
+                return hash_value.clone();
+            }
+            Node::Branch {
+                children,
+                hash_value,
+            } => {
+                for child in children {
+                    let (Node::Leaf {
+                        hash_value: child_hash,
+                        ..
+                    }
+                    | Node::Branch {
+                        hash_value: child_hash,
+                        ..
+                    }) = child;
+                    hasher.update(child_hash);
+                }
+                *hash_value = format!("{:x}", hasher.finalize());
+                return hash_value.clone();
+            }
         }
     }
 }
